c8dcd2e3fc412a448b197a2d563a105c5a5acbc4
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-07-01 16:27:36 +0100

    feat: Update CV download link

--- components/intro.tsx
+++ components/intro.tsx
@@ -87,10 +87,10 @@ const Intro = () => {
         </Link>
         <a
           className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
-          href='/CV.pdf'
+          href='/Cieran_Nicholas_CV.pdf'
           download
         >
-          Download CV{" "}
+          Download CV
           <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
         </a>
         <a
